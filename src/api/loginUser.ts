0ae828d064e9ce8b9f5d4bb22f7c585d90ef9918
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-12 09:48:52 +0700

    [GREEN] add token

--- src/api/loginUser.ts
+++ src/api/loginUser.ts
@@ -3,7 +3,7 @@ import type { LoginUserResponse } from '@/dto/LoginUserResponse.ts'
 import type { WebResponse } from '@/dto/WebResponse.ts'
 import { ApiError } from '@/exception/ApiError.ts'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL_PROD
 
 export async function loginUser(loginUserRequest: LoginUserRequest): Promise<WebResponse<LoginUserResponse>> {
   const res = await fetch(`${BASE_URL}/auth/login`, {
@@ -14,10 +14,14 @@ export async function loginUser(loginUserRequest: LoginUserRequest): Promise<Web
     body: JSON.stringify(loginUserRequest)
   })
 
+  const json: WebResponse<LoginUserResponse> = await res.json()
+
   if (!res.ok) {
     const data: WebResponse<string> = await res.json()
     throw new ApiError(data.errors, res.status, data.errors)
   }
 
-  return res.json()
+  localStorage.setItem('token', json.data.token)
+
+  return json
 }
