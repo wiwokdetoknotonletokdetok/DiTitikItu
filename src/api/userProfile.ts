5e4f7a843c3c78ce7e97cf7da37eba030df2fa84
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-14 11:36:42 +0700

    [GREEN] add logout

--- src/api/userProfile.ts
+++ src/api/userProfile.ts
@@ -2,7 +2,7 @@ import type {UserProfileResponse} from '@/dto/UserProfileResponse.ts'
 import type {WebResponse} from '@/dto/WebResponse.ts'
 import { ApiError } from '@/exception/ApiError.ts'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL_PROD
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function userProfile(userId: string): Promise<WebResponse<UserProfileResponse>> {
   const res = await fetch(`${BASE_URL}/users/${userId}`, {
@@ -18,4 +18,21 @@ export async function userProfile(userId: string): Promise<WebResponse<UserProfi
   }
 
   return res.json()
+}
+
+export async function logout(): Promise<void> {
+  const res = await fetch(`${BASE_URL}/auth/logout`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  })
+
+  if (!res.ok) {
+    const data: WebResponse<string> = await res.json()
+    throw new ApiError(data.errors, res.status, data.errors)
+  }
+
+  localStorage.removeItem('token')
 }
