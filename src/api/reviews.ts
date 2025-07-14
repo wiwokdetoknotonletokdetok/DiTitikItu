137dd8a5640115c9b6497e497598c320cb486271
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-14 11:37:08 +0700

    [GREEN] Add delete and update review

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -1,24 +1,29 @@
 import type { ReviewResponseDTO } from '@/dto/ReviewResponseDTO'
 import type { ReviewRequestDTO } from '@/dto/ReviewRequestDTO'
 import type { WebResponse } from '@/dto/WebResponse'
+import { ApiError } from '@/exception/ApiError.ts'
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
+const BASE_URL = import.meta.env.VITE_API_BASE_URL_LOCAL
 
 export async function fetchReviews(bookId: string): Promise<ReviewResponseDTO[]> {
   const res = await fetch(`${BASE_URL}/books/${bookId}/reviews`)
-  if (!res.ok) throw new Error('Gagal fetch review')
+
+  if (!res.ok) {
+    const data: WebResponse<string> = await res.json()
+    console.error('Error fetching reviews:', data.errors)
+    throw new ApiError(data.errors, res.status, data.errors)
+  }
+  
   const json: WebResponse<ReviewResponseDTO[]> = await res.json()
   return json.data
 }
 
 export async function postReview(bookId: string, review: ReviewRequestDTO): Promise<void> {
-  const token = localStorage.getItem('token')
-  console.log('Token:', token)
   const res = await fetch(`${BASE_URL}/books/${bookId}/reviews`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     },
     body: JSON.stringify(review)
   })
@@ -26,5 +31,35 @@ export async function postReview(bookId: string, review: ReviewRequestDTO): Prom
   if (!res.ok) {
     const errText = await res.text()
     throw new Error(`Gagal kirim review: ${res.status} - ${errText}`)
+  }
+}
+
+export async function deleteReview(bookId: string, userId: string) {
+  const res = await fetch(`${BASE_URL}/books/${bookId}/reviews/${userId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
+  })
+
+  if (!res.ok) {
+    const data: WebResponse<string> = await res.json()
+    throw new ApiError(data.errors, res.status, data.errors)
+  }
+}
+
+export async function updateReview(bookId: string, userId: string, payload: { message: string, rating: number }) {
+  const res = await fetch(`${BASE_URL}/books/${bookId}/reviews/${userId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`
+    },
+    body: JSON.stringify(payload)
+  })
+
+  if (!res.ok) {
+    const data: WebResponse<string> = await res.json()
+    throw new ApiError(data.errors, res.status, data.errors)
   }
 }
