9e9920bb51506b1d546674de203ad9c73c2b2595
kaachiii <kaachi579@gmail.com>
2025-07-16 21:52:39 +0700

    menambahkan link ke books di navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,6 +13,8 @@ export default function Navbar() {
   return (
     <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
       <Link to="/" style={{ marginRight: '1rem' }}>Beranda</Link>
+      <Link to="/books" style={{ marginRight: '1rem' }}>Books</Link>
+
       {userId ? (
         <>
           <Link to={`/profile/${userId}`} style={{ marginRight: '1rem' }}>Profil</Link>
