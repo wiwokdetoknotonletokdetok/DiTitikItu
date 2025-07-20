95ac8b87b45f802743b86e8584b740e8a11c173f
FransiscaEllyaB <ellyafransiscab@gmail.com>
2025-07-20 13:26:06 +0700

    [REFACTOR] fetch location

--- src/api/bookLocation.ts
+++ src/api/bookLocation.ts
@@ -5,14 +5,14 @@ import { ApiError } from '@/exception/ApiError.ts'
 
 const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-export async function fetchBookLocations(bookId: string): Promise<BookLocationResponse[]> {
-  const res = await fetch(`${BASE_URL}/books/${bookId}/locations`)
+export async function fetchBookLocations(bookId: string, latitude: number, longitude: number): Promise<BookLocationResponse[]> {
+  const res = await fetch(`${BASE_URL}/books/${bookId}/locations?latitude=${latitude}&longitude=${longitude}`)
 
   if (!res.ok) {
     const data: WebResponse<string> = await res.json()
     throw new ApiError(data.errors, res.status, data.errors)
   }
-  
+
   const data: WebResponse<BookLocationResponse[]> = await res.json()
   return data.data
 }
